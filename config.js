e6026b2f2cc823f732cd1614a91cb55298787a25
Jodenson <alertejodenson@gmail.com>
2024-12-28 21:29:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ function convertToBool(text, fault = 'true') {
 //--------------------------Mr Frank---------------------------------------
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SUBZERO SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "https://github.com/mrfrank-ofc/SUBZERO-MD/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
 OWNER_NUMBER: process.env.OWNER_NUMBER || "18062212660",
 MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
