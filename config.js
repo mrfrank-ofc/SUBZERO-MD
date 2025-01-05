ce068e9a70e70c802fbe6a9c6d6a253e218e0e77
Ralph07-alt <florvilralph6@gmail.com>
2025-01-05 15:57:26 -0500

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your subzero md session id here",
+SESSION_ID: process.env.SESSION_ID || "+5095561194",
  
 PREFIX: process.env.PREFIX || ".",
 // Enter Your Desired Prefix
